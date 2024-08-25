4db21c7a8a7b992ed9dcff811df9c8cef19c589a
Mariia Zhuravleva <167981362+Zhuravleva0701@users.noreply.github.com>
2024-08-25 22:22:29 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = '7473045798:AAHyEHGVsLAncnXCH4suZ7QkMXbc6HRhEhk'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 

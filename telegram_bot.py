86d6389cd31376619a9fa4c7f364a804a654acc0
Mariia Zhuravleva <167981362+Zhuravleva0701@users.noreply.github.com>
2024-08-27 15:26:47 +0300

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,21 +1,60 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
 import asyncio
 
-api = ''
+api = '7473045798:AAHyEHGVsLAncnXCH4suZ7QkMXbc6HRhEhk'
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text=['Calories'])
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5
+    await message.answer(f'Ваша женская норма каллорий: {result}')
+    await state.finish()
+
+'''Упрощенный вариант формулы Миффлина-Сан Жеора:
+ 
+ для мужчин: 10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5;
+ для женщин: 10 x вес (кг) + 6,25 x рост (см) – 5 x возраст (г) – 161.'''
+
+
 @dp.message_handler(commands=['start'])
 async def start(message):
-    # print('Привет! Я бот помогающий твоему здоровью.')
-    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью. Введите "Calories"')
 
 
 @dp.message_handler()
 async def all_massages(message):
-    # print('Введите команду /start, чтобы начать общение.')
     await message.answer('Введите команду /start, чтобы начать общение.')
 
 

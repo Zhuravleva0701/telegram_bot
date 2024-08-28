7460586c459c51aac65ee317f8506dc95d6db66c
Mariia Zhuravleva <167981362+Zhuravleva0701@users.noreply.github.com>
2024-08-28 23:11:48 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-api = '7473045798:AAHyEHGVsLAncnXCH4suZ7QkMXbc6HRhEhk'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
